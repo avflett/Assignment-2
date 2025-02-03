cd674c579ef556aaeba13297c1dc9f1a929170b4
avflett <aflett3@academic.rrc.ca>
2025-02-02 22:34:41 -0600

    Declare and Use Simple Data Types

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Introduction to Python: Documentation, Variables, Printing
 ## Author
 Amber Flett
 
-##
+# 1. Update the README File
 - Following predefined standards ensure coders are consistent and efficient providing quality code.
 - Developers can employ ethical habits by 
 - 
--- flett_a_assignment-2.py
+++ flett_a_assignment-2.py
@@ -1,1 +1,19 @@
 """A module to demonstrate content from Module 2."""
+
+# SIMPLE DATA TYPES
+
+my_name = "Amber"
+type = str
+print(f"name:", my_name, "type:", type)
+
+has_license = True
+type = bool
+print(f"has license:", has_license, "type:", type)
+
+this_year = 2025
+type = int
+print(f"this year:", this_year, "type:", type)
+
+next_year = 2026
+type = bool
+print(f"next year:", next_year, "type:", type)
